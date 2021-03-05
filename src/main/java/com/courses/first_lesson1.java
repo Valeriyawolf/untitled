9063fb34e568c8470b58ff6302701ceec6223db1
valeriyaxs17 <wolf.valeriya@gmail.com>
2021-03-05 18:11:23 +0200

    test1

---
+++ src/main/java/com/courses/first_lesson1.java
@@ -0,0 +1,21 @@
+package com.courses;
+import WebDriveManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class first_lesson1 {
+public static void main (String [] args) {
+
+  // System.setProperty("webdriver.chrome.driver", "scr/main/resources/chromedriver.exe");
+   // WebDriver driver = new ChromeDriver();
+  //  driver.get ("http://google.com.ua");
+
+    WebDriverManager.chromedriver();
+    WebDriver driver = new ChromeDriver();
+
+
+  }
+
+}
