ecb89eced724c7f43f5fb68f24c41def56f621ed
valeriyaxs17 <wolf.valeriya@gmail.com>
2021-03-08 00:31:46 +0200

    Open https://www.google.com/ test1

--- src/main/java/com/courses/first_lesson1.java
+++ src/main/java/com/courses/first_lesson1.java
@@ -10,47 +10,54 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
 public class first_lesson1 {
-public static void main  (String [] args) {
-    WebDriverManager.chromedriver().setup();
-    WebDriver driver = new ChromeDriver();
-    driver.get("http://saucedemo.com");
-    WebElement usernameField = driver.findElement(By.id("user-name"));
-    WebElement passwordField = driver.findElement(By.id("password"));
-    WebElement loginButton = driver.findElement(By.id("login-button"));
 
-    usernameField.sendKeys("standard_user");
-    passwordField.sendKeys("secret_sauce");
-    loginButton.click();
+    public static void main(String[] args) {
+        int indexForReversionFilter = 1;
 
-    List<WebElement> list = driver.findElements(By.xpath("//button"));
-    WebElement addToCart = list.get(3);
-    addToCart.click();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
 
-    WebElement filter = driver.findElement(By.className("product_sort_container"));
-    Select select = new Select(filter);
-
-    List<WebElement> listOfOptions = select.getOptions();
+        WebElement usernameField = driver.findElement(By.className("form_input"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[2]"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
 
 
+        usernameField.sendKeys("standard_user");
+        passwordField.sendKeys("secret_sauce");
+        loginButton.click();
+
+        List<WebElement> list = driver.findElements(By.xpath("//button"));
+        WebElement addToCart2 = list.get(3);
+        addToCart2.click();
+
+        WebElement filter = driver.findElement(By.className("product_sort_container"));
+        Select select = new Select(filter);
+
+        List<WebElement> listOfOptions = select.getOptions();
+
+        for(int i = 0; i < listOfOptions.size(); i++) {
+            System.out.println(listOfOptions.get(i).getText());
+        }
+
+        select.selectByIndex(indexForReversionFilter);
+        select.selectByValue("za");
+        select.selectByVisibleText("Name (Z to A)");
+
+        //WebElement list = driver.findElement(By.className("product_sort_container"));
+        ///List<WebElement> list = driver.findElements(By.xpath("//button"));
+        ///WebElement addToCart3 = list.get(4);
+        ///addToCart3.click();
+
+
+        WebElement burgerButton = driver.findElement (By.id("react-burger-menu-btn"));
+        WebElement logoutLink = driver.findElement(By.id("logout_sidebar_link"));
+        burgerButton.click();
+        logoutLink.click();
 
 
 
-    for(int i = 0; i < listOfOptions.size(); i++) {
-
-        System.out.println(listOfOptions.get(i).getText());
+        //driver.quit();
 
     }
-
-    select.selectByIndex(Integer.parseInt("indexForReversionFilter"));
-    select.selectByValue("lohi");
-    select.selectByVisibleText("Price (high to low)");
-
-    driver.quit();
-
-
-
-
-
-}
-
 }
