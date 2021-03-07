98e30778d00fd2e53203406b53b19e7208d8e40a
valeriyaxs17 <wolf.valeriya@gmail.com>
2021-03-07 21:42:25 +0200

    Open https://www.google.com/

--- src/main/java/com/courses/first_lesson1.java
+++ src/main/java/com/courses/first_lesson1.java
@@ -4,16 +4,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class first_lesson1 {
 public static void main (String [] args) {
 
- // System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-  //  WebDriver driver = new ChromeDriver();
-   //driver.get ("http://google.com.ua");
+  System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+   WebDriver driver = new ChromeDriver();
+   driver.get ("http://google.com.ua");
 
-    WebDriverManager.chromedriver();
+  //  WebDriverManager.chromedriver();
+  //  WebDriverManager.chromedriver();
    // WebDriver driver = new ChromeDriver();
    // WebDriver driver = new ChromeDriver();
 
