ffa8165c35e7ad6771409f0e47d77a73ef89e3b2
valeriyaxs17 <wolf.valeriya@gmail.com>
2021-03-17 22:35:08 +0200

    Homework5

---
+++ src/main/java/Helpers/BasePages.java
@@ -0,0 +1,25 @@
+package Helpers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BasePages {
+    protected WebDriver driver;
+
+
+    @BeforeMethod
+    protected void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
